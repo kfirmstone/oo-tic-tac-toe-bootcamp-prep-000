a6c97873a98cb54f7a8d2e43fb3b166b262786d0
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-02-22 20:05:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -148,11 +148,5 @@ end
     @board.count{|token| token == "X" || token == "O"}
   end
  
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
+  
 end
